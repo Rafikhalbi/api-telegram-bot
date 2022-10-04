cc797477311a77ae0f52740a97814c59e59b4d8e
Rafi Khalbi <89525492+Rafikhalbi@users.noreply.github.com>
2022-10-04 20:54:47 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import telebot
+from re import search
+from requests import get
+
+token = '5490361434:AAEwMGledRVEqWBfiSVECyqBHzuF3-0lyro'
+bot = telebot.TeleBot(token, parse_mode=None)
+@bot.message_handler()
+def send_botMessage(message):
+    messtxt = message.text.replace('/simi ','')
+    get_ = get(f'https://api.simsimi.net/v2/?text={messtxt}&lc=id').text
+    messsimi = search(r'{"methods":"GET","success":"(.*?)","noti":"MeoCop#5555"',str(get_))[1]
+    bot.reply_to(message, messsimi)
+
+bot.infinity_polling()
